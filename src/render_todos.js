9a82cdb9b3578e3ed5c1386ee4e3de1f926b2d56
Liger <jtadub@gmail.com>
2020-07-30 12:37:46 +1200

    Add grid for todos

--- src/render_todos.js
+++ src/render_todos.js
@@ -9,29 +9,49 @@ const todosRenderer = (() => {
     let todoDiv = document.createElement('div');
     todoDiv.classList.add('todo-div');
 
+    let todoTitleDiv = document.createElement('div');
+    todoTitleDiv.classList.add('todo-title-div');
+
     let todoTitle = document.createElement('h1');
     todoTitle.classList.add('todo-title');
     todoTitle.textContent = todo.title;
+    todoTitleDiv.appendChild(todoTitle);
 
+    let todoContentDiv = document.createElement('div');
+    todoContentDiv.classList.add('todo-content-div');
+
+    let todoDueDateDiv = document.createElement('div');
+    todoDueDateDiv.classList.add('todo-due-date-div');
     let todoDueDate = document.createElement('p') 
     todoDueDate.classList.add('todo-due-date');
     todoDueDate.textContent = todo.dueDate;
+    todoDueDateDiv.appendChild(todoDueDate);
 
+    let todoDescriptionDiv = document.createElement('div');
+    todoDescriptionDiv.classList.add('todo-description-div');
     let todoDescription = document.createElement('p');
     todoDescription.classList.add('todo-description');
     todoDescription.textContent = todo.description;
+    todoDescriptionDiv.appendChild(todoDescription);
+
+    let todoButtonsDiv = document.createElement('div');
+    todoButtonsDiv.classList.add('todo-buttons-div');
 
     let todoProjectType = document.createElement('button');
     todoProjectType.classList.add('todo-project-type');
+    todoProjectType.classList.add('todo-button');
+    todoButtonsDiv.appendChild(todoProjectType);
 
     let removeTodo = document.createElement('button');
     removeTodo.classList.add('remove-todo');
+    removeTodo.classList.add('todo-button');
+    todoButtonsDiv.appendChild(removeTodo);
 
-    todoDiv.appendChild(todoTitle);
-    todoDiv.appendChild(todoDueDate);
-    todoDiv.appendChild(todoDescription);
-    todoDiv.appendChild(todoProjectType);
-    todoDiv.appendChild(removeTodo);
+    todoDiv.appendChild(todoTitleDiv);
+    todoContentDiv.appendChild(todoDueDateDiv);
+    todoContentDiv.appendChild(todoDescriptionDiv);
+    todoContentDiv.appendChild(todoButtonsDiv);
+    todoDiv.appendChild(todoContentDiv);
 
     return todoDiv;
   }
